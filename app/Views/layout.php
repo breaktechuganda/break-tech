e6ef32142d0b93be72ab82bee315cf341b1b8ecf
breaktechuganda <breaktechuganda@gmail.com>
2024-10-30 17:35:44 +0530

    Code Testing

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Break Tech hds || <?= $title ?></title>
+    <title>Break Tech hds | <?= $title ?></title>
     <link
       rel="apple-touch-icon"
       sizes="180x180"
